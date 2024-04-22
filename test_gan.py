c7786345189cb82b8a4f93fd32180afb8ef7faf1
tungdt <duongthanhtungvn01@gmai.com>
2024-04-22 16:17:37 +0700

    first commit

---
+++ test_gan.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np
+import cv2
+import tensorflow as tf
+from tensorflow.keras.layers import Add, BatchNormalization, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.vgg19 import VGG19    
+
+def normalize_01(x):
+    """Normalizes RGB images to [0, 1]."""
+    return x / 255.0
+
+def denormalize_m11(x):
+    """Inverse of normalize_m11."""
+    return (x + 1) * 127.5
+
+def pixel_shuffle(scale):
+    return lambda x: tf.nn.depth_to_space(x, scale)
+
+def res_block(x_in, num_filters, momentum=0.8):
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+    x = BatchNormalization(momentum=momentum)(x)
+    x = PReLU(shared_axes=[1, 2])(x)
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+    x = BatchNormalization(momentum=momentum)(x)
+    x = Add()([x_in, x])
+    return x
+
+def upsample(x_in, num_filters):
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+    x = Lambda(pixel_shuffle(scale=2))(x)
+    return PReLU(shared_axes=[1, 2])(x)
+
+def create_model(num_filters=64, num_res_blocks=16):
+    x_in = Input(shape=(64, 64, 3))
+    x = Lambda(normalize_01)(x_in)
+
+    x = Conv2D(num_filters, kernel_size=9, padding='same')(x)
+    x = x_1 = PReLU(shared_axes=[1, 2])(x)
+
+    for _ in range(num_res_blocks):
+        x = res_block(x, num_filters)
+
+    x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Add()([x_1, x])
+
+    x = upsample(x, num_filters * 4)
+    x = upsample(x, num_filters * 4)
+
+    x = Conv2D(3, kernel_size=9, padding='same', activation='tanh')(x)
+    x = Lambda(denormalize_m11)(x)
+    return Model(x_in, x)
+    
+model = create_model()
+model.load_weights('gan_weights.h5')
+images = []
+files = os.listdir('input')
+files = sorted(files)[:100]
+for f in files:
+    img = cv2.imread('input/'+f)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    images.append(img)
+    
+output = model(np.array(images))
+output = tf.clip_by_value(output, 0, 255)
+output = tf.round(output)
+output = tf.cast(output, tf.uint8).numpy()
+
+for f,out_img in zip(files, output):
+    out_img = cv2.cvtColor(out_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('out/'+f, out_img)
